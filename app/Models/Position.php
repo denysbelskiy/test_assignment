846ba20512c6ec3f99c18b1c799d66633a5d5193
denysbelskiy <denys.belskiy@powercoders.org>
2025-03-23 18:33:12 +0200

    feat/relationship definition

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -11,7 +11,7 @@ class Position extends Model
 
     protected $fillable = ['name'];
 
-    public function talents()
+    public function users()
     {
         return $this->hasMany(User::class);
     }
